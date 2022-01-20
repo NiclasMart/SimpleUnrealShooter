bf45f252eb52b258cfdbddc0435c3ca5438b3045
Niclas Mart <salcintram07@web.de>
2022-01-20 10:57:17 +0100

    some balancing and enemy positioning changes

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -1,5 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ShooterPlayerController.h"
 
@@ -44,7 +44,4 @@ void AShooterPlayerController::ShowEndscreen(bool bIsWinner)
 	bShowMouseCursor = true;
 	SetInputMode(FInputModeUIOnly());
 	LoseScreen->AddToViewport();
-	
-
-	
 }
